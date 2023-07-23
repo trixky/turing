93ee744b95a5829b12ce297d7e50fc19f6f8df62
Mathis Bois <mathis.bois@outlook.fr>
2023-07-23 13:10:02 +0200

    [Dev] description: add the informations about the description

---
+++ src/lib/components/DescriptionInformations.svelte
@@ -0,0 +1,74 @@
+<!-- ================================================ SCRIPT -->
+
+<!-- ================================================ CONTENT -->
+<section class="description-container">
+	<h2>Description</h2>
+	<ul>
+		<li>
+			<p><b>name</b>: The name of the described machine.</p>
+		</li>
+		<li>
+			<p>
+				<b>alphabet</b>: Both input and work alphabet of the machine merged into a single alphabet
+				for simplicity’s sake, including the blank character. Each character of the alphabet must be
+				a string of length strictly equal to 1.
+			</p>
+		</li>
+		<li>
+			<p><b>blank</b>: The blank character, must be part of the alphabet.</p>
+		</li>
+		<li>
+			<p><b>states</b>: The exhaustive list of the machine’s states names.</p>
+		</li>
+		<li>
+			<p><b>initial</b>: The initial state of the machine, must be part of the states list.</p>
+		</li>
+		<li>
+			<p>
+				<b>finals</b>: The sub-list from states of the machine’s final states.
+			</p>
+		</li>
+		<li>
+			<p>
+				<b>transitions</b>: A dictionnary of the machine’s transitions indexed by state name. Each
+				transition is a list of dictionnaries, and each dictionnary describes the transition for a
+				given character under the head of the machine.
+			</p>
+			<ul>
+				<li>
+					<p>
+						<b>read</b>: The character of the machine’s alphabet on the tape under the machine’s
+						head.
+					</p>
+				</li>
+				<li>
+					<p><b>to_state</b>: The new state of the machine after the transition is done.</p>
+				</li>
+				<li>
+					<p>
+						<b>write</b>: The character of the machine’s alphabet to write on the tape before moving
+						the head.
+					</p>
+				</li>
+				<li>
+					<p><b>action</b>: Movement of the head for this transition, either LEFT, or RIGHT.</p>
+				</li>
+			</ul>
+		</li>
+	</ul>
+    <p class="sneaky">The description use the <b>JSON</b> format</p>
+</section>
+
+<!-- ================================================ STYLE -->
+<style>
+	section,
+
+    .description-container > p {
+        float: right;
+    }
+
+	ul > li > p {
+		margin: 0;
+		padding: 0;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,37 +2,35 @@
 <script lang="ts">
 	import SpeedStore from '$lib/stores/speed';
 	import StripStore from '$lib/stores/strip';
-    import DescriptionStore from '$lib/stores/description';
-    import StateStore from '$lib/stores/state';
+	import DescriptionStore from '$lib/stores/description';
+	import StateStore from '$lib/stores/state';
 
-    let input_description_value = ""
+    import DescriptionInformations from '$lib/components/DescriptionInformations.svelte'
 
-    function handle_load_description() {
-        try {
-            DescriptionStore.load(input_description_value)
-        } catch (error) {
-            alert(error)
-        }
-        StripStore.init($DescriptionStore.blank)
-        StateStore.set($DescriptionStore.initial)
-    }
+	let input_description_value = '';
+
+	function handle_load_description() {
+		try {
+			DescriptionStore.load(input_description_value);
+		} catch (error) {
+			alert(error);
+		}
+		StripStore.init($DescriptionStore.blank);
+		StateStore.set($DescriptionStore.initial);
+	}
 </script>
 
 <!-- ================================================ CONTENT -->
 
 <h1>Turing</h1>
 
-<p>
-	Description Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa assumenda modi adipisci
-	eaque rerum, dolore reprehenderit, veritatis velit illum deleniti labore placeat vel ipsam
-	dolorum. Ut aut fuga magni. Ab?
-</p>
 
+<DescriptionInformations />
 <section>
 	<form>
 		<div class="description-container">
-			<textarea bind:value={input_description_value}/>
-            <button on:click={handle_load_description}>Load</button>
+			<textarea bind:value={input_description_value} />
+			<button on:click={handle_load_description}>Load</button>
 		</div>
 		<div>
 			<label for="speed">speed</label>
@@ -56,26 +54,27 @@
 
 <!-- ================================================ STYLE -->
 <style>
-    section, form {
-        width: 100%;
-    }
+	section,
+	form {
+		width: 100%;
+	}
 
-    form .description-container {
-        display: flex;
-        flex-direction: column;
-    }
+	form .description-container {
+		display: flex;
+		flex-direction: column;
+	}
 
-    form textarea {
-        width: 100%;
-        height: 200px;
-        resize: none;
-    }
+	form textarea {
+		width: 100%;
+		height: 200px;
+		resize: none;
+	}
 
 	ol {
 		display: flex;
 	}
 
-	li {
+	ol > li {
 		display: flex;
 		justify-content: center;
 		align-items: center;
